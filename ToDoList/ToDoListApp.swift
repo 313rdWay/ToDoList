463ad44c19d704b942c1614c31523f98c462e849
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-03-04 14:21:53 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Davaughn Williams on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
